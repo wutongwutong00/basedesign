fbbc9be4fa62e6d0f9a26d4803b1be98757190da
wutongwutong00 <98603275+wutongwutong00@users.noreply.github.com>
2024-10-08 16:51:27 -0400

    updates
    on main page and links

---
+++ script.js
@@ -0,0 +1,8 @@
+// window.open(url, '', 'width=1000, height=500')
+
+let huey = document.querySelector(".huey");
+function openHuey() {
+    window.open("popup", 'width=1000, height=500')
+}
+
+console.log('huey is selected');
