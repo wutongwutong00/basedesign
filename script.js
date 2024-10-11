088e21e0f3df4bc85721a40d973bf678f2b5ad75
wutongwutong00 <98603275+wutongwutong00@users.noreply.github.com>
2024-10-11 10:07:44 -0400

    huey live update

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function openHuey() {
     window1.document.write("<img src='https://raw.githubusercontent.com/wutongwutong00/basedesign/main/resource/images/huey2.jpg' width='400'/>");
 
     var window2 = window.open("", "", 'width=500, height=500')
-    window2.document.write("<img src='resource/images/huey5.png' width='500'/>");
+    window2.document.write("<img src='https://raw.githubusercontent.com/wutongwutong00/basedesign/main/resource/images/huey5.png' width='500'/>");
 
     var window3 = window.open("", "", 'width=200, height=200')
-    window3.document.write("<img src='resource/images/huey1.jpg' width='200'/>");
+    window3.document.write("<img src='https://raw.githubusercontent.com/wutongwutong00/basedesign/main/resource/images/huey1.jpg' width='200'/>");
 
     tmot1 = setTimeout(function(){window1.close()}, 4000);
     tmot2 = setTimeout(function(){window2.close()}, 3000);
